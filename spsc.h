5092a7320dc9a62f33adc7916d9b284d59f6f26d
Jack Copland <jack@jackcodeland.com>
2025-04-17 01:03:41 +1000

    add name space, change requirements

--- spsc.h
+++ spsc.h
@@ -1,63 +1,65 @@
 #ifndef JC_SPSC_H
 #define JC_SPSC_H
 
-#include <algorithm>
 #include <array>
 #include <atomic>
 #include <cstddef>
 #include <bit>
+#include <optional>
+#include <type_traits>
 
-template<std::size_t sz>
-concept is_power_of_two = std::has_single_bit(sz);
+using std::size_t;
 
-template<typename T, std::size_t sz = 512>
-    requires is_power_of_two<sz>
-class spsc
-{
-private:
-    alignas(64)std::atomic<ssize_t> writer_ = 0;
-    alignas(64)std::atomic<ssize_t> reader_ = 0;
-    alignas(64)std::array<T, sz> items;
+namespace SPSC {
 
-public:
-    spsc() = default;
-    void force_put(T&& element)
+    template<size_t sz>
+    concept is_power_of_two = std::has_single_bit(sz);
+
+    template<typename T, size_t sz = 512> requires
+    is_power_of_two<sz> &&
+    std::is_move_constructible_v<T> &&
+    std::is_move_assignable_v<T> &&
+    std::is_trivially_destructible_v<T>
+    class spsc
     {
-        ssize_t idx = writer_.load(std::memory_order::relaxed);
-        items[idx & (sz - 1)] = std::move(element);
-        writer_.store(idx + 1, std::memory_order::release);
-    }
+        private:
+        alignas(64)std::atomic<size_t> writer_ = 0;
+        alignas(64)std::atomic<size_t> reader_ = 0;
+        alignas(64)std::array<T, sz> items;
 
-    bool try_put(T&& element)
-    {
-        ssize_t idx = writer_.load(std::memory_order::relaxed);
-        if (idx - reader_.load(std::memory_order::acquire) == sz)
+        public:
+        spsc() = default;
+
+        spsc(const spsc&) = delete;
+        spsc& operator=(const spsc&) = delete;
+        spsc(spsc&&) = delete;
+        spsc& operator=(spsc&&) = delete;
+
+        ~spsc() = default;
+
+        bool put(T&& element)
         {
-            return false;
+            size_t idx = writer_.load(std::memory_order::relaxed);
+            if (idx - reader_.load(std::memory_order::acquire) == sz)
+            {
+                return false;
+            }
+            items[idx & (sz - 1)] = std::move(element);
+            writer_.store(idx + 1, std::memory_order::release);
+            return true;
         }
-        items[idx & (sz - 1)] = std::move(element);
-        writer_.store(idx + 1, std::memory_order::release);
-        return true;
-    }
 
-    bool try_read(T& element)
-    {
-        ssize_t idx = reader_.load(std::memory_order::relaxed);
-        if (idx == writer_.load(std::memory_order::acquire))
+        std::optional<T> read()
         {
-            return false;
+            size_t idx = reader_.load(std::memory_order::relaxed);
+            if (idx == writer_.load(std::memory_order::acquire))
+            {
+                return {};
+            }
+            T element = std::move(items[idx & (sz - 1)]);
+            reader_.store(idx + 1, std::memory_order::release);
+            return std::make_optional(element);
         }
-        element = std::move(items[idx & (sz - 1)]);
-        reader_.store(idx + 1, std::memory_order::release);
-        return true;
-    }
-
-    void force_read(T& element)
-    {
-        ssize_t idx = reader_.load(std::memory_order::relaxed);
-        element = std::move(items[idx & (sz - 1)]);
-        reader_.store(idx + 1, std::memory_order::release);
-    }
-
-};
+    };
+}
 #endif
