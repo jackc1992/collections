4ec1e92db867207a79539f902a73971d8e59635f
Jack Copland <jack@jackcodeland.com>
2025-04-19 17:34:55 +1000

    more benches :)

--- benchmarks/src/simple_bm.cpp
+++ benchmarks/src/simple_bm.cpp
@@ -1,114 +1,114 @@
-#include <jc_collections/spsc.hpp>
-
-#define _GNU_SOURCE
-#include <pthread.h>
-#include <sched.h>
-#include <unistd.h>
-#include <vector>
-#include <print>
-#include <thread>
-#include <numeric>
-
-using std::size_t;
-
-void set_thread_affinity(int core_id) {
-    cpu_set_t cpu_set;
-    CPU_ZERO(&cpu_set);
-    CPU_SET(core_id, &cpu_set);
-
-    pthread_t current_thread = pthread_self();
-    pthread_setaffinity_np(current_thread, sizeof(cpu_set_t), &cpu_set);
-}
-
-constexpr size_t get_size(size_t floor)
-{
-    size_t res = 1 ;
-    while (res < floor)
-    {
-        res *= 2;
-    }
-    return res;
-}
-
-struct alignas(4) TestSize 
-{
-    int x_;
-};
-
-
-int main()
-{
-    const size_t iters = get_size(5'000'000);
-    const size_t trials = 5;
-
-    std::println("testing jc_spsc, iters: {}", iters);
-
-    std::vector<size_t> ops(trials);
-    std::vector<size_t> rtt(trials);
-
-
-    for (size_t i = 0; i < trials; i++)
-    {
-        auto q = JC_SPSC::cached_spsc<TestSize, 4096>();
-        auto thread = std::thread([&]() {
-            set_thread_affinity(2);
-            for (size_t i = 0; i < iters; i++)
-            {
-                TestSize val = q.read();
-            }
-        });
-
-        set_thread_affinity(8);
-        auto start = std::chrono::steady_clock::now();
-        for (int i = 0; i < iters; i++)
-        {
-            q.put(TestSize{i});
-        }
-        thread.join();
-
-        auto stop = std::chrono::steady_clock::now();
-
-        ops[i] =
-        iters * 1'000'000 /
-        std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start)
-            .count();
-    }
-
-    for (size_t i = 0; i < trials; i++)
-    {
-        auto q1 = JC_SPSC::cached_spsc<TestSize, 4096>();
-        auto q2 = JC_SPSC::cached_spsc<TestSize, 4096>();
-
-        auto thread = std::thread([&]() {
-            set_thread_affinity(2);
-            for (size_t i = 0; i < iters; i++)
-            {
-                TestSize val = q1.read();
-                q2.put(std::move(val));
-            }
-        });
-
-        set_thread_affinity(8);
-        auto start = std::chrono::steady_clock::now();
-        for (int i = 0; i < iters; i++)
-        {
-            q1.put(TestSize{i});
-            TestSize val = q2.read();
-        }
-
-        thread.join();
-        auto stop = std::chrono::steady_clock::now();
-
-        rtt[i] = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start).count() / iters;
-    }
-
-    std::sort(ops.begin(), ops.end());
-    std::sort(rtt.begin(), rtt.end());
-
-    std::println("Mean: {} ops/ms", std::accumulate(ops.begin(), ops.end(), 0) / trials);
-    std::println("Median: {} ops/ms", ops[trials / 2]);
-    std::println("Mean: {} RTT in ns", std::accumulate(rtt.begin(), rtt.end(), 0) / trials);
-    std::println("Mean: {} RTT in ns", rtt[trials / 2]);
-
-    return 0;
-}
+// #include <jc_collections/spsc.hpp>
+//
+// #define _GNU_SOURCE
+// #include <pthread.h>
+// #include <sched.h>
+// #include <unistd.h>
+// #include <vector>
+// #include <print>
+// #include <thread>
+// #include <numeric>
+//
+// using std::size_t;
+//
+// void set_thread_affinity(int core_id) {
+//     cpu_set_t cpu_set;
+//     CPU_ZERO(&cpu_set);
+//     CPU_SET(core_id, &cpu_set);
+//
+//     pthread_t current_thread = pthread_self();
+//     pthread_setaffinity_np(current_thread, sizeof(cpu_set_t), &cpu_set);
+// }
+//
+// constexpr size_t get_size(size_t floor)
+// {
+//     size_t res = 1 ;
+//     while (res < floor)
+//     {
+//         res *= 2;
+//     }
+//     return res;
+// }
+//
+// struct alignas(8) TestSize
+// {
+//     int x_;
+// };
+//
+//
+// int main()
+// {
+//     const size_t iters = get_size(5'000'000);
+//     const size_t trials = 5;
+//
+//     std::println("testing jc_spsc, iters: {}", iters);
+//
+//     std::vector<size_t> ops(trials);
+//     std::vector<size_t> rtt(trials);
+//
+//
+//     for (size_t i = 0; i < trials; i++)
+//     {
+//         auto q = JC_SPSC::cached_spsc<TestSize, 2048>();
+//         auto thread = std::thread([&]() {
+//             set_thread_affinity(2);
+//             for (size_t i = 0; i < iters; i++)
+//             {
+//                 TestSize val = q.read();
+//             }
+//         });
+//
+//         set_thread_affinity(8);
+//         auto start = std::chrono::high_resolution_clock::now();
+//         for (int i = 0; i < iters; i++)
+//         {
+//             q.put(TestSize{i});
+//         }
+//         thread.join();
+//
+//         auto stop = std::chrono::high_resolution_clock::now();
+//
+//         ops[i] =
+//         iters * 1'000'000 /
+//         std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start)
+//             .count();
+//     }
+//
+//     for (size_t i = 0; i < trials; i++)
+//     {
+//         auto q1 = JC_SPSC::cached_spsc<TestSize, 2048>();
+//         auto q2 = JC_SPSC::cached_spsc<TestSize, 2048>();
+//
+//         auto thread = std::thread([&]() {
+//             set_thread_affinity(2);
+//             for (size_t i = 0; i < iters; i++)
+//             {
+//                 TestSize val = q1.read();
+//                 q2.put(std::move(val));
+//             }
+//         });
+//
+//         set_thread_affinity(8);
+//         auto start = std::chrono::high_resolution_clock::now();
+//         for (int i = 0; i < iters; i++)
+//         {
+//             q1.put(TestSize{i});
+//             TestSize val = q2.read();
+//         }
+//
+//         thread.join();
+//         auto stop = std::chrono::high_resolution_clock::now();
+//
+//         rtt[i] = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start).count() / iters;
+//     }
+//
+//     std::sort(ops.begin(), ops.end());
+//     std::sort(rtt.begin(), rtt.end());
+//
+//     std::println("Mean: {} ops/ms", std::accumulate(ops.begin(), ops.end(), 0) / trials);
+//     std::println("Median: {} ops/ms", ops[trials / 2]);
+//     std::println("Mean: {} RTT in ns", std::accumulate(rtt.begin(), rtt.end(), 0) / trials);
+//     std::println("Mean: {} RTT in ns", rtt[trials / 2]);
+//
+//     return 0;
+// }
--- benchmarks/src/spsc_bm.cpp
+++ benchmarks/src/spsc_bm.cpp
@@ -1,70 +1,98 @@
-// #include <benchmark/benchmark.h>
-// #include <jc_collections/spsc.hpp>
+#include <benchmark/benchmark.h>
+#include <jc_collections/spsc.hpp>
 
-// #define _GNU_SOURCE
-// #include <pthread.h>
-// #include <sched.h>
-// #include <unistd.h>
+#define _GNU_SOURCE
+#include <pthread.h>
+#include <sched.h>
+#include <thread>
+#include <print>
 
-// void set_thread_affinity(int core_id) {
-//     cpu_set_t cpu_set;
-//     CPU_ZERO(&cpu_set);
-//     CPU_SET(core_id, &cpu_set);
+void set_thread_affinity(int core_id) {
+#ifdef __linux__
+    cpu_set_t cpu_set;
+    CPU_ZERO(&cpu_set);
+    CPU_SET(core_id, &cpu_set);
 
-//     pthread_t current_thread = pthread_self();
-//     pthread_setaffinity_np(current_thread, sizeof(cpu_set_t), &cpu_set);
-// }
+    pthread_t current_thread = pthread_self();
+    pthread_setaffinity_np(current_thread, sizeof(cpu_set_t), &cpu_set);
 
-// struct bigger {
-//     size_t a[32];
-// };
+#endif
+}
 
-// static void bm_spsc_cached_throughput(benchmark::State& state)
-// {
-//     int core_id = (state.thread_index() == 0) ? 2 : 9;
+struct alignas(4) bigger {
+    int x_;
+};
 
-//     set_thread_affinity(core_id);
+static void bm_spsc_cached_throughput(benchmark::State& state)
+{
+    const int core_id = (state.thread_index() == 0) ? 2 : 3;
 
-//     static JC_SPSC::cached_spsc<bigger, 2048> q;
+    set_thread_affinity(core_id);
+    static JC_SPSC::cached_spsc<bigger, 512> q;
 
-//     if (state.thread_index() == 0) {
-//         // Producer thread loop
-//         for (auto _ : state) {
-//             q.put(bigger { 0 });
-//         }
-//     } else {
-//         // Consumer thread loop
-//         for (auto _ : state) {
-//             bigger val = q.read();
-//             benchmark::DoNotOptimize(val);
-//         }
-//     }
-// }
+    if (state.thread_index() == 0) {
+        // Producer thread loop
+        for (auto _ : state) {
+            q.put(bigger { 0 });
+        }
+    } else {
+        // Consumer thread loop
+        for (auto _ : state) {
+            bigger val = q.read();
+            benchmark::DoNotOptimize(val);
+        }
+    }
+}
 
 
-// static void bm_spsc_throughput(benchmark::State& state)
-// {
-//     int core_id = (state.thread_index() == 0) ? 2 : 3;
+static void bm_spsc_throughput(benchmark::State& state)
+{
+    const int core_id = (state.thread_index() == 0) ? 2 : 3;
 
-//     set_thread_affinity(core_id);
+    set_thread_affinity(core_id);
 
-//     static JC_SPSC::simple_spsc<size_t, 2048> q;
+    static JC_SPSC::simple_spsc<bigger, 2048> q;
 
-//     if (state.thread_index() == 0) {
-//         // Producer thread loop
-//         for (auto _ : state) {
-//             q.put(1);
-//         }
-//     } else {
-//         // Consumer thread loop
-//         for (auto _ : state) {
-//             int val = q.read();
-//             benchmark::DoNotOptimize(val);
-//         }
-//     }
-// }
+    if (state.thread_index() == 0) {
+        // Producer thread loop
+        for (auto a : state) {
+            q.put(bigger { 1 });
+        }
+    } else {
+        // Consumer thread loop
+        for (auto _ : state) {
+            auto val = q.read();
+            benchmark::DoNotOptimize(val);
+        }
+    }
+}
 
-// BENCHMARK(bm_spsc_throughput)->Threads(2)->UseRealTime()->Iterations(100000000);
-// BENCHMARK(bm_spsc_cached_throughput)->Threads(2)->UseRealTime()->MinTime(1.0);
+template<typename T>
+static void bm_rtt_throughput(benchmark::State& state) {
+    static auto q1 = JC_SPSC::cached_spsc<T, 512>();
+    static auto q2 = JC_SPSC::cached_spsc<T, 512>();
 
-// BENCHMARK_MAIN();
+    if (state.thread_index() == 0) {
+        set_thread_affinity(3);
+        for (auto _ : state) {
+            q1.put(T());
+            T ele = q2.read();
+            benchmark::DoNotOptimize(ele);
+        }
+    } else {
+        set_thread_affinity(2);
+        for (auto _ : state) {
+            T ele = q1.read();
+            benchmark::DoNotOptimize(ele);
+            q2.put(std::move(ele));
+        }
+    }
+    state.SetItemsProcessed(state.iterations());
+}
+
+BENCHMARK(bm_spsc_cached_throughput)->Threads(2)->UseRealTime()->MinTime(1.0);
+BENCHMARK(bm_spsc_throughput)->Threads(2)->UseRealTime()->Iterations(100000000);
+BENCHMARK(bm_rtt_throughput<size_t>)->Threads(2)->UseRealTime()->Range((1 << 16), (1 << 22));
+
+
+BENCHMARK_MAIN();
